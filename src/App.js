5806e20d29948ce3ab525b0f0d1616dd51c100d0
ManojH <68124449+laviManoj@users.noreply.github.com>
2023-07-19 12:36:35 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+
+const FileUploadForm = () => {
+  const [file1, setFile1] = useState(null);
+  const [file2, setFile2] = useState(null);
+  const [file1Preview, setFile1Preview] = useState(null);
+  const [file2Preview, setFile2Preview] = useState(null);
+
+  const handleFile1Change = (event) => {
+    const selectedFile = event.target.files[0];
+    setFile1(selectedFile);
+
+    // Generate preview URL for File 1
+    const reader = new FileReader();
+    reader.onload = () => {
+      setFile1Preview(reader.result);
+    };
+    reader.readAsDataURL(selectedFile);
+  };
+
+  const handleFile2Change = (event) => {
+    const selectedFile = event.target.files[0];
+    setFile2(selectedFile);
+
+    // Generate preview URL for File 2
+    const reader = new FileReader();
+    reader.onload = () => {
+      setFile2Preview(reader.result);
+    };
+    reader.readAsDataURL(selectedFile);
+  };
+
+  const handleSubmit = () => {
+    const formData = new FormData();
+    formData.append("file1", file1);
+    formData.append("file2", file2);
+
+    fetch("http://localhost:5002/upload-files", {
+      method: "POST",
+      body: formData
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Files uploaded successfully:", data);
+      })
+      .catch((error) => {
+        console.error("Error uploading files:", error);
+      });
+  };
+
+  return (
+    <div>
+      <h1>File Upload</h1>
+      <form>
+        <div>
+          <label>Choose File 1:</label>
+          <input type="file" onChange={handleFile1Change} />
+          {file1Preview && <img src={file1Preview} alt="File 1 Preview" />}
+        </div>
+
+        <div>
+          <label>Choose File 2:</label>
+          <input type="file" onChange={handleFile2Change} />
+          {file2Preview && <img src={file2Preview} alt="File 2 Preview" />}
+        </div>
+
+        <button type="button" onClick={handleSubmit}>
+          Upload
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default FileUploadForm;
